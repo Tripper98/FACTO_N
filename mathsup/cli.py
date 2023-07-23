cafb52a2bac8ce7da262a56ba986ae2632a977c9
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-07-23 17:01:09 +0200

    fix: fix the cli.py file

--- mathsup/cli.py
+++ mathsup/cli.py
@@ -3,11 +3,12 @@ import core
 
 def main():
     parser = argparse.ArgumentParser(description="Calculate the factorial of a number.")
-    parser.add_argument("number", type=int, help="The number for which to calculate the factorial.")
+    parser.add_argument("--number", "-n", type=int, help="The number for which to calculate the factorial.")
     args = parser.parse_args()
 
-    result = core.factorial(args.number)
-    print(f"The factorial of {args.number} is {result}.")
+    if args.number:
+        result = core.factorial(args.number)
+        print(f"The factorial of {args.number} is {result}.")
 
 
 
